620bf83600ffc2064b15318cb0fa8d759c19b992
jeremygit <jheritage@gmail.com>
2020-09-30 13:48:57 +1000

    add a simple/fakse auth mechanisms

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BLEContext, BLEContextProvider } from './contexts/BLEContext';
+import { AuthContext, AuthContextProvider } from './contexts/AuthContext';
 
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
@@ -10,17 +11,21 @@ import { AppBar, Toolbar } from '@material-ui/core';
 
 function App() {
   return (
+    <AuthContextProvider>
     <BLEContextProvider>
       <div className="App">
         <AppMain />
       </div>
     </BLEContextProvider>
+    </AuthContextProvider>
   );
 }
 
 function AppMain() {
 
   const bleContext = useContext(BLEContext);
+
+  const authContext = useContext(AuthContext);
 
   const onClickScan = (evt) => {
     bleContext.startScan(evt);
@@ -39,6 +44,7 @@ function AppMain() {
       <AppBar position="fixed">
         <Toolbar>
           <h2>Hikerlocker {bleContext.connectionState}</h2>
+          <AuthToolbar/>
         </Toolbar>
       </AppBar>
       <Toolbar/>
@@ -68,6 +74,27 @@ function AppMain() {
         </Box>
       </Container>
     </>
+  )
+}
+
+const AuthToolbar = () => {
+
+  const authContext = useContext(AuthContext);
+
+  const onLogoutClick = () => {
+    authContext.logout();
+  }
+
+  const onLoginClick = () => {
+    authContext.login();
+  }
+
+  return (
+    authContext.initialized
+    ? authContext.authUser
+      ? <Button onClick={onLogoutClick}>{authContext.authUser.username} (Logout)</Button>
+      : <Button onClick={onLoginClick}>Login</Button>
+    : <span>...</span>
   )
 }
 
---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,44 @@
+import React, { createContext, useEffect, useState } from 'react';
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+
+  const [state, setState] = useState({
+    initialized: false,
+  });
+
+  const [authUser, setAuthUser] = useState(null);
+
+  const logout = () => {
+    setAuthUser(null);
+  }
+
+  const login = () => {
+    setAuthUser({
+      username: 'Test User',
+      uuid: '1b9d6bcd-cafe-dead-beef-ab8dfbbd4bed',
+    });
+  }
+
+  useEffect(() => {
+
+    login();
+
+    setState({initialized: true});
+
+    return () => {}
+  }, []);
+
+  return (
+    <AuthContext.Provider value={
+        {...state, 
+          authUser: authUser,
+          logout: logout,
+          login: login,
+        }
+      }>
+      {children}
+    </AuthContext.Provider> 
+  );
+};
